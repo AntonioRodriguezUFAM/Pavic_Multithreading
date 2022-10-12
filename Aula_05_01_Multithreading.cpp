5f8ba6e31faceb052a81836011e74e876530037a
AntonioRodriguezUFAM <antonio.rodrigue@icomp.ufam.edu.br>
2022-10-11 20:54:38 -0400

    Add project files.

---
+++ Aula_05_01_Multithreading.cpp
@@ -0,0 +1,25 @@
+// Pavic Multithreading C++ - 2022
+
+#include<iostream>
+#include<thread>
+using namespace std;
+
+void call_from_thread() {
+	cout << "Hello, World from Thread" << endl;
+}
+
+int main() {
+
+	// Launch a thread
+
+	thread t1(call_from_thread);
+	cout << "Hello, World from CPU" << endl;
+
+	t1.join();
+	cout << "Hello, World from CPU - after Join" << endl;
+
+	return 0;
+
+
+
+}
