a23f9960d93151da1cc4d7aeac41cc6409d7a961
AntonioRodriguezUFAM <antonio.rodrigue@icomp.ufam.edu.br>
2022-10-11 21:23:16 -0400

    thread Antonio Added

--- Aula_05_01_Multithreading.cpp
+++ Aula_05_01_Multithreading.cpp
@@ -1,22 +1,37 @@
 // Pavic Multithreading C++ - 2022
 
+// std::cout / printf!!
 #include<iostream>
+
+// create Threads
 #include<thread>
-using namespace std;
+
+
+//using namespace std; // Removed std::
+
+void Antonio() {
+	printf("Antonio threads \n");
+}
 
 void call_from_thread() {
-	cout << "Hello, World from Thread" << endl;
+	std::cout << "Hello, World from Thread" << std::endl;
 }
+
+// Code Main
 
 int main() {
 
 	// Launch a thread
 
-	thread t1(call_from_thread);
-	cout << "Hello, World from CPU" << endl;
+	std::thread t1(call_from_thread);
+	std::thread antonio(Antonio);
 
+	std::cout << "Hello, World from CPU" << std::endl;
+
+	antonio.join();
 	t1.join();
-	cout << "Hello, World from CPU - after Join" << endl;
+
+	std::cout << "Hello, World from CPU - after Join" << std::endl;
 
 	return 0;
 
